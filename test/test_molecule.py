32b0a01672b25faa3e0a500598a78429acd1d21f
mohanson <mohanson@outlook.com>
2024-01-19 13:39:17 +0800

    2024-01-19 13:39:17

--- test/test_molecule.py
+++ test/test_molecule.py
@@ -4,4 +4,4 @@ import ckb
 def test_bytenn():
     a = ckb.molecule.Bytenn(bytearray([0x00, 0x01]))
     b = ckb.molecule.Bytenn.molecule_read(a.molecule())
-    assert a.data == b.data
+    assert a.data == b
