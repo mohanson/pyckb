05737df85e53ecee3b02714baa87c0c7a1241451
mohanson <mohanson@outlook.com>
2024-03-27 09:46:17 +0800

    Optimize examples

--- example/addr.py
+++ example/addr.py
@@ -4,8 +4,17 @@ import ckb
 # Calculate address from private key in secp256k1 lock.
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--net', type=str, choices=['develop', 'mainnet', 'testnet'], default='testnet')
 parser.add_argument('prikey', type=str, help='private key')
 args = parser.parse_args()
+
+if args.net == 'develop':
+    ckb.config.upgrade('http://127.0.0.1:8114')
+    ckb.config.current = ckb.config.develop
+if args.net == 'mainnet':
+    ckb.config.current = ckb.config.mainnet
+if args.net == 'testnet':
+    ckb.config.current = ckb.config.testnet
 
 prikey = ckb.core.PriKey(int(args.prikey, 0))
 pubkey = prikey.pubkey()
--- example/capacity.py
+++ example/capacity.py
@@ -4,8 +4,17 @@ import ckb
 # Get the capacity by an address.
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--net', type=str, choices=['develop', 'mainnet', 'testnet'], default='testnet')
 parser.add_argument('addr', type=str, help='address')
 args = parser.parse_args()
+
+if args.net == 'develop':
+    ckb.config.upgrade('http://127.0.0.1:8114')
+    ckb.config.current = ckb.config.develop
+if args.net == 'mainnet':
+    ckb.config.current = ckb.config.mainnet
+if args.net == 'testnet':
+    ckb.config.current = ckb.config.testnet
 
 capacity = int(ckb.rpc.get_cells_capacity({
     'script': ckb.core.address_decode(args.addr).json(),
