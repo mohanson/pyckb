eadb6d37d8dce278960d90654f312519e010e0b7
mohanson <mohanson@outlook.com>
2025-02-22 10:41:13 +0800

    Update ecdsa.py

--- pyckb/ecdsa.py
+++ pyckb/ecdsa.py
@@ -27,12 +27,11 @@ def sign(prikey: pyckb.secp256k1.Fr, m: pyckb.secp256k1.Fr) -> typing.Tuple[pyck
 def verify(pubkey: pyckb.secp256k1.Pt, m: pyckb.secp256k1.Fr, r: pyckb.secp256k1.Fr, s: pyckb.secp256k1.Fr) -> bool:
     # https://www.secg.org/sec1-v2.pdf
     # 4.1.4 Verifying Operation
-    u1 = m / s
-    u2 = r / s
-    x = pyckb.secp256k1.G * u1 + pubkey * u2
-    assert x != pyckb.secp256k1.I
-    v = pyckb.secp256k1.Fr(x.x.x)
-    return v == r
+    a = m / s
+    b = r / s
+    R = pyckb.secp256k1.G * a + pubkey * b
+    assert R != pyckb.secp256k1.I
+    return r == pyckb.secp256k1.Fr(R.x.x)
 
 
 def pubkey(m: pyckb.secp256k1.Fr, r: pyckb.secp256k1.Fr, s: pyckb.secp256k1.Fr, v: int) -> pyckb.secp256k1.Pt:
@@ -43,8 +42,8 @@ def pubkey(m: pyckb.secp256k1.Fr, r: pyckb.secp256k1.Fr, s: pyckb.secp256k1.Fr, 
         x = pyckb.secp256k1.Fq(r.x)
     else:
         x = pyckb.secp256k1.Fq(r.x + pyckb.secp256k1.N)
-    y_y = x * x * x + pyckb.secp256k1.A * x + pyckb.secp256k1.B
-    y = y_y ** ((pyckb.secp256k1.P + 1) // 4)
+    z = x * x * x + pyckb.secp256k1.A * x + pyckb.secp256k1.B
+    y = z ** ((pyckb.secp256k1.P + 1) // 4)
     if v & 1 != y.x & 1:
         y = -y
     R = pyckb.secp256k1.Pt(x, y)
